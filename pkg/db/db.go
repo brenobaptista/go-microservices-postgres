0e494b29f63aa6931386ddbc8baf7dd690ff1974
Breno Baptista <brenobaptista@protonmail.com>
2021-12-16 21:04:03 -0300

    Set up new db

---
+++ pkg/db/db.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"fmt"
+
+	"github.com/go-pg/migrations/v8"
+	"github.com/go-pg/pg/v10"
+)
+
+func NewDB() (*pg.DB, error) {
+	db := pg.Connect(&pg.Options{
+		Addr:     "db:5432",
+		User:     "postgres",
+		Password: "admin",
+	})
+
+	collection := migrations.NewCollection()
+	err := collection.DiscoverSQLMigrations("migrations")
+	if err != nil {
+		return nil, err
+	}
+
+	_, _, err = collection.Run(db, "init")
+	if err != nil {
+		return nil, err
+	}
+
+	oldVersion, newVersion, err := collection.Run(db, "up")
+	if err != nil {
+		return nil, err
+	}
+	if newVersion != oldVersion {
+		fmt.Printf("migrated from version %d to %d\n", oldVersion, newVersion)
+	} else {
+		fmt.Printf("version is %d\n", oldVersion)
+	}
+
+	return db, nil
+}
