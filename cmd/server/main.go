13eeda1f5614ac32e74a51191eac8f498458671a
Breno Baptista <brenobaptista@protonmail.com>
2021-12-16 21:04:29 -0300

    Initialized new db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"fmt"
 	"go-microservices-postgres/pkg/api"
+	"go-microservices-postgres/pkg/db"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	_, err := db.NewDB()
+	if err != nil {
+		panic(err)
+	}
+
 	router := api.NewAPI()
 
 	log.Print("we are up and running!")
 	port := os.Getenv("PORT")
-	err := http.ListenAndServe(fmt.Sprintf(":%s", port), router)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", port), router)
 	if err != nil {
 		log.Printf("error from router: %v\n", err)
 	}
