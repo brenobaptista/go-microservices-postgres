ba2c07656eb196e712a378f645ed5b1e22d8894c
Breno Baptista <brenobaptista@protonmail.com>
2021-12-16 22:08:45 -0300

    Implemented CreateHome method

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,16 +1,20 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
+	"go-microservices-postgres/pkg/db"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-pg/pg/v10"
 )
 
-func NewAPI() *chi.Mux {
+func NewAPI(pgdb *pg.DB) *chi.Mux {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	r.Use(middleware.Logger, middleware.WithValue("DB", pgdb))
 
 	r.Route("/homes", func(r chi.Router) {
 		r.Post("/", createHome)
@@ -27,8 +31,78 @@ func NewAPI() *chi.Mux {
 	return r
 }
 
+type CreateHomeRequest struct {
+	Price       int64  `json:"price"`
+	Description string `json:"description"`
+	Address     string `json:"address"`
+	AgentID     int64  `json:"agent_id"`
+}
+
+type CreateHomeResponse struct {
+	Success bool     `json:"success"`
+	Error   string   `json:"error"`
+	Home    *db.Home `json:"home"`
+}
+
 func createHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("create home\n"))
+	req := &CreateHomeRequest{}
+	err := json.NewDecoder(r.Body).Decode(req)
+	if err != nil {
+		res := &CreateHomeResponse{
+			Success: false,
+			Error:   err.Error(),
+			Home:    nil,
+		}
+		err = json.NewEncoder(w).Encode(res)
+		if err != nil {
+			log.Printf("error sending response: %v\n", err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	pgdb, ok := r.Context().Value("DB").(*pg.DB)
+	if !ok {
+		res := &CreateHomeResponse{
+			Success: false,
+			Error:   "could not get database from context",
+			Home:    nil,
+		}
+		err = json.NewEncoder(w).Encode(res)
+		if err != nil {
+			log.Printf("error sending response: %v\n", err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	home, err := db.CreateHome(pgdb, &db.Home{
+		Price:       req.Price,
+		Description: req.Description,
+		Address:     req.Address,
+		AgentID:     req.AgentID,
+	})
+	if err != nil {
+		res := &CreateHomeResponse{
+			Success: false,
+			Error:   err.Error(),
+			Home:    nil,
+		}
+		err = json.NewEncoder(w).Encode(res)
+		if err != nil {
+			log.Printf("error sending response: %v\n", err)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	res := &CreateHomeResponse{
+		Success: true,
+		Error:   "",
+		Home:    home,
+	}
+	_ = json.NewEncoder(w).Encode(res)
+	w.WriteHeader(http.StatusOK)
 }
 
 func getHomeByID(w http.ResponseWriter, r *http.Request) {
